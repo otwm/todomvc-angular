6023bc5e894d68897f59ea5be666631f59567c70
kdo <otwmoo@gmail.com>
2016-06-25 14:23:03 +0900

    호스팅 설정

--- Memo.md
+++ Memo.md
@@ -4,3 +4,4 @@
 몽고, express, node , angular
 
 * node server/app.js
+* express는 미들웨어를 이용해 여러가지 것들을 서비스 한다.
--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+//static 설정
+app.use('/', express.static(path.join(__dirname, '../client')));
+
+//node module 설정
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
 
 /**
  * 라우팅
  */
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendfile('index.html');
 });
 
 app.listen(3000, function () {
