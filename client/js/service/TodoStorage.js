0986804d1b3438af340efb4473a864227f3dcc88
kdo <otwmoo@gmail.com>
2016-06-25 15:54:35 +0900

    post 메서드를 서버 버젼으로 컨버젼

--- client/js/service/TodoStorage.js
+++ client/js/service/TodoStorage.js
@@ -19,16 +19,16 @@ angular.module("todomvc").factory('todoStorage', function ($http) {
             }
         },
         post: function (newTodoTitle) {
-            var todos = storage.todos;
-            var newTodoId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
-            todos.push({
-                id: newTodoId,
-                title: newTodoTitle,
-                completed: false
+            var body = {
+                title: newTodoTitle
+            };
+            $http.post('/api/todos', body).then(function success(response) {
+                storage.todos.push(response.data);
             });
         },
         get: function (callback) {
             $http.get('/api/todos').then(function success(response) {
+                storage.todos = response.data;
                 callback(response.data);
             });
             return storage.todos;
